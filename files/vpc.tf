704bdf6d9b224f0962ab13bb3325058f833f939a
cerfort2 <122559164+cerfort2@users.noreply.github.com>
2023-05-09 10:50:14 -0400

    Create vpc.tf
    Creating vpc.tf

---
+++ files/vpc.tf
@@ -0,0 +1,12 @@
+network_name=guarav-network
+
+projectid=var.project
+
+
+subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
